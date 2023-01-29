0c700b2559eced44803963d18c547f6331ae8cd5
Rory Collins <rorycollins@me.com>
2023-01-29 15:32:48 +0000

    add country select to the form

--- src/components/PaymentForm/PaymentForm.tsx
+++ src/components/PaymentForm/PaymentForm.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import styles from "./PaymentForm.module.css";
-import { useForm } from "react-hook-form";
-import { PaymentInputsWrapper, usePaymentInputs } from "react-payment-inputs";
+import { useForm, Controller } from "react-hook-form";
+import Select from "react-select";
+import countryList from "react-select-country-list";
 
 export const PaymentForm = ({ onBack }: any) => {
   const {
@@ -9,6 +10,7 @@ export const PaymentForm = ({ onBack }: any) => {
     handleSubmit,
     reset,
     formState: { errors },
+    control,
   } = useForm({
     defaultValues: {
       cardNumber: "",
@@ -16,17 +18,16 @@ export const PaymentForm = ({ onBack }: any) => {
       ccv: "",
       addressLineOne: "",
       addressLineTwo: "",
+      country: "",
     },
   });
 
-  // const {
-  //   meta,
-  //   getCardImageProps,
-  //   getCardNumberProps,
-  //   getExpiryDateProps,
-  //   getCVCProps,
-  //   wrapperProps,
-  // } = usePaymentInputs();
+  const [value, setValue] = useState("");
+  const options = useMemo(() => countryList().getData(), []);
+
+  const changeHandler = (value: any) => {
+    setValue(value);
+  };
 
   const handleSubmission = (data: any) => {
     console.log(
@@ -39,66 +40,85 @@ export const PaymentForm = ({ onBack }: any) => {
       onSubmit={handleSubmit((data) => handleSubmission(data))}
       className={styles.formContainer}>
       <h2>Update payment method</h2>
-
-      {/* <PaymentInputsWrapper {...wrapperProps}> */}
-      <label
-        style={{
-          border: "1px solid grey",
-          paddingBottom: "5px",
-          outline: "red",
-        }}
-        className={styles.cardInput}>
-        <input
-          // {...getCardNumberProps({ onChange: () => {} })}
-          {...register("cardNumber", {
-            required: "Card number is required",
-          })}
-          placeholder="Card number"
-          style={{ width: "16em", border: "none", outline: "transparent" }}
+      <section>
+        <label
+          style={{
+            border: "1px solid grey",
+            paddingBottom: "5px",
+            outline: "red",
+          }}
+          className={styles.cardInput}>
+          <input
+            {...register("cardNumber", {
+              required: "Card number is required",
+            })}
+            placeholder="Card number"
+            style={{ width: "16em", border: "none", outline: "transparent" }}
+          />
+          <input
+            {...register("expiryDate", {
+              required: "Expiry date is required",
+            })}
+            placeholder="MM/YY"
+            style={{ width: "6em", border: "none", outline: "transparent" }}
+          />
+          <input
+            {...register("ccv", {
+              required: "CCV is required",
+            })}
+            placeholder={"CCV"}
+            style={{ width: "3em", border: "none", outline: "transparent" }}
+          />
+        </label>
+      </section>
+      <section>
+        <label className={styles.addressLabel}>
+          Address Line 1
+          <input
+            // id={`title-${idea?.id}`}
+            {...register("addressLineOne", {
+              required: "Address line 1 is required",
+            })}
+            placeholder="e.g. 123 Fake St."
+            required
+            className={styles.addressInput}
+            // defaultValue={idea?.title}
+            // aria-label={idea?.title}
+          />
+        </label>
+      </section>
+      <section>
+        <label className={styles.addressLabel}>
+          Address Line 2
+          <input
+            // id={`title-${idea?.id}`}
+            {...register("addressLineTwo", {
+              required: "Address line 2 is required",
+            })}
+            placeholder="e.g. 123 Fake St."
+            className={styles.addressInput}
+            // defaultValue={idea?.title}
+            // aria-label={idea?.title}
+          />
+        </label>
+      </section>
+      <section>
+        <label>Country</label>
+        <Controller
+          name="country"
+          control={control}
+          render={({ value }: any) => (
+            <Select
+              className={styles.countryInput}
+              options={options}
+              value={value}
+              onChange={changeHandler}
+              // defaultValue={country.find((c) => c.value === countryValue)}
+            />
+          )}
+          rules={{ required: true }}
         />
-        <input
-          // {...getCardNumberProps({ onChange: () => {} })}
-          {...register("expiryDate", {
-            required: "Expiry date is required",
-          })}
-          placeholder="MM/YY"
-          style={{ width: "6em", border: "none", outline: "transparent" }}
-        />
-        <input
-          // {...getCardNumberProps({ onChange: () => {} })}
-          {...register("ccv", {
-            required: "CCV is required",
-          })}
-          placeholder={"CCV"}
-          style={{ width: "3em", border: "none", outline: "transparent" }}
-        />
-      </label>
-      {/* </PaymentInputsWrapper> */}
-      <label>
-        <input
-          // id={`title-${idea?.id}`}
-          {...register("addressLineOne", {
-            required: "Address line 1 is required",
-          })}
-          placeholder="e.g. 123 Fake St."
-          required
-          className={styles.addressInput}
-          // defaultValue={idea?.title}
-          // aria-label={idea?.title}
-        />
-      </label>
-      <label>
-        <input
-          // id={`title-${idea?.id}`}
-          {...register("addressLineTwo", {
-            required: "Address line 2 is required",
-          })}
-          placeholder="e.g. 123 Fake St."
-          className={styles.addressInput}
-          // defaultValue={idea?.title}
-          // aria-label={idea?.title}
-        />
-      </label>
+      </section>
       <div>
         <button onClick={onBack}>Cancel</button>
       </div>
