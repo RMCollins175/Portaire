95f60e2786cd335a7af3c868b4c2e3f41b15aafe
Rory Collins <rorycollins@me.com>
2023-01-30 15:50:05 +0000

    add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,21 @@ import { PaymentHome } from "./components/PaymentHome/PaymentHome";
 
 const API = "https://portaireapi.herokuapp.com/test/payment";
 
+interface ApiResponse {
+  address_one: number;
+  address_two: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  post_code: string;
+  state: string;
+  __v: number;
+  _id: string;
+}
+
 function App() {
   const [currentScreen, setCurrentScreen] = useState<string>("home");
-  const [formData, setFormData] = useState<any>(null);
+  const [formData, setFormData] = useState<ApiResponse[] | null>(null);
 
   const handleBackButton = () => {
     setCurrentScreen("home");
@@ -29,7 +41,10 @@ function App() {
       {currentScreen === "home" ? (
         <PaymentHome setCurrentScreen={setCurrentScreen} />
       ) : (
-        <PaymentForm onBack={handleBackButton} formData={formData} />
+        <PaymentForm
+          onBack={handleBackButton}
+          formData={formData ? formData[0] : null}
+        />
       )}
     </div>
   );
